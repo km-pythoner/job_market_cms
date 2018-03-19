2e18f8822f6938098bcff7317dd9350d4d837540
1137829446@qq.com <jj19901030>
2018-03-19 18:05:52 +0800

    后台管理开发

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.db.models import Q
 from django.views.generic.base import View
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.backends import ModelBackend
 
 from .forms import RegisterForm, LoginForm
@@ -66,5 +66,10 @@ class LoginView(View):
         else:
             return render(request, 'index.html')
 
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return render(request, 'index.html')
 
 
